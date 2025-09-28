21ba9b3dde6855a549d617ae37b6dfe07f8b4ece
Katya <ater.93@mail.ru>
2025-09-28 16:32:12 +0300

    add animation from fragments transition

--- app/src/main/java/com/example/notepad/view/MainActivity.kt
+++ app/src/main/java/com/example/notepad/view/MainActivity.kt
@@ -173,6 +173,10 @@ class MainActivity : AppCompatActivity() {
         val fragment = NoteDetailFragment.newInstance(noteId)
         supportFragmentManager
             .beginTransaction()
+            .setCustomAnimations(R.anim.slide_in_right,
+                R.anim.slide_out_left,
+                R.anim.slide_in_left,
+                R.anim.slide_out_right)
             .replace(R.id.fragment_container, fragment)
             .addToBackStack(null)
             .commit()
